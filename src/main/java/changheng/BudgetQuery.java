412ba260e913c54a8aa090f3d105ba529d940179
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2019-06-27 10:20:33 +0800

    :recycle: rename and inline variable

--- src/main/java/changheng/BudgetQuery.java
+++ src/main/java/changheng/BudgetQuery.java
@@ -9,43 +9,48 @@ import java.util.stream.Collectors;
 
 public class BudgetQuery {
     public Double query(LocalDate from, LocalDate to) {
-        List<Budget> budgetList = getAllBudgets();
+        List<Budget> budgets = getAllBudgets();
         if (from.isAfter(to)) {
             throw new IllegalArgumentException("Invalid date range");
         }
 
         double result = 0;
-        if (sameYearMonth(from, to)) {
-            int diff = Period.between(from, to).getDays() + 1;
-            List<Budget> queryResult = query(getBeginDayDate(from), getEndDayDate(to), budgetList);
+        if (isSameYearMonth(from, to)) {
+            List<Budget> queryResult = query(getBeginDayDate(from), getEndDayDate(to), budgets);
             for (Budget budget : queryResult) {
-                result += budget.getAmount() / getMonthLength(from) * diff;
+                result += budget.getAmount() / getMonthLength(from) * intervalDays(from, to);
             }
             return result;
         }
 
         // calc first month
         int diff = getMonthLength(from) - from.getDayOfMonth() + 1;
-        List<Budget> queryResult = query(getBeginDayDate(from), getEndDayDate(from), budgetList);
+        List<Budget> queryResult = query(getBeginDayDate(from), getEndDayDate(from), budgets);
         for (Budget budget : queryResult) {
             result += budget.getAmount() / getMonthLength(from) * diff;
         }
         from = from.plusMonths(1);
         from = LocalDate.of(from.getYear(), from.getMonthValue(), 1);
 
-        for (LocalDate now = from; now.isBefore(to) && !sameYearMonth(now, to); now = now.plusMonths(1)) {
-            queryResult = query(now, getEndDayDate(now), budgetList);
+        for (LocalDate now = from; now.isBefore(to) && !isSameYearMonth(now, to); now = now.plusMonths(1)) {
+            queryResult = query(now, getEndDayDate(now), budgets);
             for (Budget budget : queryResult) {
                 result += budget.getAmount();
             }
         }
         // calc last month
         diff = to.getDayOfMonth();
-        queryResult = query(LocalDate.of(to.getYear(), to.getMonthValue(), 1), getEndDayDate(to), budgetList);
+        queryResult = query(LocalDate.of(to.getYear(), to.getMonthValue(), 1), getEndDayDate(to), budgets);
         for (Budget budget : queryResult) {
             result += budget.getAmount() / getMonthLength(to) * diff;
         }
         return result;
+    }
+
+    protected List<Budget> getAllBudgets() {
+        Budget a = new Budget("2019/02", 28000d);
+        Budget b = new Budget("2019/03", 31000d);
+        return Arrays.asList(a, b);
     }
 
     private LocalDate getBeginDayDate(LocalDate d) {
@@ -56,24 +61,22 @@ public class BudgetQuery {
         return LocalDate.of(d.getYear(), d.getMonthValue(), getMonthLength(d));
     }
 
+    private int getMonthLength(LocalDate date) {
+        return YearMonth.of(date.getYear(), date.getMonth()).lengthOfMonth();
+    }
+
+    private int intervalDays(LocalDate from, LocalDate to) {
+        return Period.between(from, to).getDays() + 1;
+    }
+
+    private boolean isSameYearMonth(LocalDate from, LocalDate to) {
+        return YearMonth.from(from).equals(YearMonth.from(to));
+    }
+
     private List<Budget> query(LocalDate from, LocalDate to, List<Budget> budgetList) {
         return budgetList.stream().filter(budget -> {
             LocalDate d = budget.getLocalDate();
             return (d.isAfter(from) || d.isEqual(from)) && (d.isBefore(to) || d.isEqual(to));
         }).collect(Collectors.toList());
-    }
-
-    private int getMonthLength(LocalDate date) {
-        return YearMonth.of(date.getYear(), date.getMonth()).lengthOfMonth();
-    }
-
-    private boolean sameYearMonth(LocalDate a, LocalDate b) {
-        return a.getMonthValue() == b.getMonthValue() && a.getYear() == b.getYear();
-    }
-
-    protected List<Budget> getAllBudgets() {
-        Budget a = new Budget("2019/02", 28000d);
-        Budget b = new Budget("2019/03", 31000d);
-        return Arrays.asList(a, b);
     }
 }
